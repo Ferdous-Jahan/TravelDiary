ec360fdfab1f030b8cbd3c489469bc0ab6d4913c
Ferdous Jahan <ferdousjahan2@gmail.com>
2020-03-18 00:03:20 +0600

    index dynamic view

--- index.php
+++ index.php
@@ -7,6 +7,12 @@
   if (!isset($id)) {
     header('Location: login.php');
   }
+
+  $query = 'SELECT * FROM posts';
+  $result = mysqli_query($conn, $query);
+  $posts = mysqli_fetch_all($result, MYSQLI_ASSOC);
+  mysqli_free_result($result);
+  mysqli_close($conn);
 ?>
 <!DOCTYPE html>
 
@@ -19,63 +25,23 @@
         <i title="Upload" class="fa fa-plus-circle fa-4x" aria-hidden="true"></i>
       </a>
     </div>
-    <p style="margin: 16px; display: flex; justify-content: center;">Share your experience</p>
+    <p style="margin: 16px; display: flex; justify-content: center;"><a href="add.php">SHARE YOUR EXPERIENCE</a></p>
   </div>
   </div>
 
-
-  <div style="display: flex; flex-wrap: wrap; justify-content: space-around;">
+  <?php foreach ($posts as $post): ?>
+    <div style="display: flex; flex-wrap: wrap; justify-content: space-around;">
     <div class="card" style="margin: 10px;">
       <a style="text-decoration: none;" href="details.php">
-        <img src="images/amikhum.jpg" alt="Avatar" style="width:100%">
+        <img src="<?php echo $post['image']; ?>" alt="Avatar" style="width:100%">
         <div class="container">
-          <h4><b>Amiakhum Waterfall</b></h4>
-          <p>Amiakhum,Thanchi,Bandarban</p>
+          <h4><b><?php echo $post['title']; ?></b></h4>
+          <p><?php echo $post['details']; ?></p>
         </div>
       </a>
     </div>
-
-    <div class="card" style="margin: 10px;">
-      <a style="text-decoration: none;" href="details.html">
-        <img src="images/amikhum.jpg" alt="Avatar" style="width:100%">
-        <div class="container">
-          <h4><b>Amiakhum Waterfall</b></h4>
-          <p>Amiakhum,Thanchi,Bandarban</p>
-        </div>
-      </a>
-    </div>
-
-    <div class="card" style="margin: 10px;">
-      <a style="text-decoration: none;" href="details.html">
-        <img src="images/amikhum.jpg" alt="Avatar" style="width:100%">
-        <div class="container">
-          <h4><b>Amiakhum Waterfall</b></h4>
-          <p>Amiakhum,Thanchi,Bandarban</p>
-        </div>
-      </a>
-    </div>
-
-    <div class="card" style="margin: 10px;">
-      <a style="text-decoration: none;" href="details.html">
-        <img src="images/amikhum.jpg" alt="Avatar" style="width:100%">
-        <div class="container">
-          <h4><b>Amiakhum Waterfall</b></h4>
-          <p>Amiakhum,Thanchi,Bandarban</p>
-        </div>
-      </a>
-    </div>
-
-    <div class="card" style="margin: 10px;">
-      <a style="text-decoration: none;" href="details.html">
-        <img src="images/amikhum.jpg" alt="Avatar" style="width:100%">
-        <div class="container">
-          <h4><b>Amiakhum Waterfall</b></h4>
-          <p>Amiakhum,Thanchi,Bandarban</p>
-        </div>
-      </a>
-    </div>
-
   </div>
+  <?php endforeach ?>
 
 </body>
 
